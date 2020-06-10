218d84d1b30be8ea5d3141c53d013864364f9bec
thms200 <thms0504@gmail.com>
2020-06-10 16:04:10 +0900

    test Add checking vingle rule

--- tests/advanced_spec.js
+++ tests/advanced_spec.js
@@ -9,9 +9,19 @@ describe("advanced validator test", () => {
     expect(isValidXML("<a></a>>")).toBeFalsy();
   });
 
-  it("should return false for an xml with a node containgin a node with the same tag", () => {
+  it("(Vingle's rules) should return false for an xml with a node containgin a node with the same tag", () => {
     expect(isValidXML("<div><div></div></div>")).toBeFalsy();
     expect(isValidXML("<span></span><div><div></div></div>")).toBeFalsy();
     expect(isValidXML("<article></article><div><div></div></div>")).toBeFalsy();
+  });
+
+  it("(Vingle's rules) should return false for an xml with consecutive nodes of the same tag", () => {
+    expect(isValidXML("<div></div><div></div>")).toBeFalsy();
+    expect(isValidXML("<div></div><article></article><article></article>")).toBeFalsy();
+  });
+
+  it("(Vingle's rules) should return false for an xml with a depth of more than 2.", () => {
+    expect(isValidXML("<a></a><b><c><d></d></c></b>")).toBeFalsy();
+    expect(isValidXML("<ul><li><span></span></li><ul>")).toBeFalsy();
   });
 });
