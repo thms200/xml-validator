18e1234eab57c3b3a5ffb9e7dee0ba9873b0cee8
thms200 <thms0504@gmail.com>
2020-06-10 15:46:23 +0900

    test: Add for checking vigle rule

--- tests/advanced_spec.js
+++ tests/advanced_spec.js
@@ -8,4 +8,10 @@ describe("advanced validator test", () => {
     expect(isValidXML("<a><</a>")).toBeFalsy();
     expect(isValidXML("<a></a>>")).toBeFalsy();
   });
+
+  it("should return false for an xml with a node containgin a node with the same tag", () => {
+    expect(isValidXML("<div><div></div></div>")).toBeFalsy();
+    expect(isValidXML("<span></span><div><div></div></div>")).toBeFalsy();
+    expect(isValidXML("<article></article><div><div></div></div>")).toBeFalsy();
+  });
 });
